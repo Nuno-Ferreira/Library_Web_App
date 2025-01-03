55834435356270fa8b19f98286a86d5af9560aa8
Nuno Ferreira <nuno.ferreira@corepower.energy>
2025-01-03 16:08:49 +0000

    Created a user profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from books.models import Book
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    country = models.CharField(max_length=100)
+    bio = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.user.username
+
+    @property
+    def books(self):
+        return Book.objects.filter(user=self.user)

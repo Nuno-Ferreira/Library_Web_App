3dfa81fb3dec642fe1e5b8f7838c5c88e2992b4d
Nuno Ferreira <nuno.ferreira@corepower.energy>
2025-01-03 15:42:36 +0000

    Added page to display book details

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.homepage, name="HomePage"),
     path("books/", views.books_index, name="BooksIndex"),
+    path("books/<int:book_id>/", views.book_detail, name="BookDetail"),
 ]
--- books/views.py
+++ books/views.py
@@ -9,3 +9,8 @@ def books_index(request):
     available_books = Book.objects.all()
 
     return render(request, "templates/book_index.html", {"books": available_books})
+
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+
+    return render(request, "templates/book_detail.html", {"book": book})

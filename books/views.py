3eb127114ebd43960d335bc50430cb94ed7f8df5
Nuno Ferreira <nuno.ferreira@corepower.energy>
2025-01-03 14:52:25 +0000

    Added home page to the web app

--- books/views.py
+++ books/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, "templates/home.html")
 
 def index(request):
     return HttpResponse("Hello, User. You're at the books index.")

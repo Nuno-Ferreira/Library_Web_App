7c9d4f03a75d6c7ab8b79fb2242472d728e95523
Nuno Ferreira <nuno.ferreira@corepower.energy>
2025-01-03 17:50:17 +0000

    Changed the user field in books model to prevent a circular import

--- books/models.py
+++ books/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey("accounts.UserProfile", on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title

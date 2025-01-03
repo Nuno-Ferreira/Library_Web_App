9027ff313d39d087b95b1a26297f155f34b69380
Nuno Ferreira <nuno.ferreira@corepower.energy>
2025-01-03 15:25:27 +0000

    Added a table in the books index to display all books

--- books/views.py
+++ books/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from books.models import Book
+
 def homepage(request):
     return render(request, "templates/home.html")
 
 def books_index(request):
-    return render(request, "templates/book_index.html")
+    available_books = Book.objects.all()
+
+    return render(request, "templates/book_index.html", {"books": available_books})

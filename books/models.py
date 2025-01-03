3d92440d0fc4c6b4cfb229bd0489722d291f024f
Nuno Ferreira <nuno.ferreira@corepower.energy>
2025-01-03 15:50:45 +0000

    Changed user field to be empty on book creation

--- books/models.py
+++ books/models.py
@@ -5,7 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
